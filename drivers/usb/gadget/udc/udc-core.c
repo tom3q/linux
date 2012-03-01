669ab9ccc7d615c2ce3ddcf4161b4d5c057aed0e
Benoit Goby <benoit@android.com>
2012-03-01 13:17:07 -0800

    HACK: usb: gadget: Fix enumeration on boot
    The Android gadget driver disconnects the gadget on bind
    and expects the gadget to stay disconnected until it calls
    usb_gadget_connect when userspace is ready. Removed the call
    to usb_gadget_connect in usb_gadget_probe_driver to avoid
    enabling the pullup before userspace is ready.
    
    Change-Id: I63707ac6e16a44eca52351a4bf80407d25fbd35e
    Signed-off-by: Benoit Goby <benoit@android.com>

--- drivers/usb/gadget/udc/udc-core.c
+++ drivers/usb/gadget/udc/udc-core.c
@@ -422,7 +422,15 @@ no_extcon:
 		goto err2;
 	}
 
-	usb_gadget_connect(udc->gadget);
+	/*
+	 * HACK: The Android gadget driver disconnects the gadget
+	 * on bind and expects the gadget to stay disconnected until
+	 * it calls usb_gadget_connect when userspace is ready. Remove
+	 * the call to usb_gadget_connect bellow to avoid enabling the
+	 * pullup before userspace is ready.
+	 *
+	 * usb_gadget_connect(udc->gadget);
+	 */
 
 	if (udc->extcon_usb_dev.edev) {
 		udc->enabled = 1;
