7d9aebb71d840f8dd3d7d3b22e6a624cd30128d6
Tomasz Figa <tfiga@chromium.org>
2018-03-11 18:22:41 +0900

    Revert "[media] media: s5p-mfc fix invalid memory access from s5p_mfc_release()"
    Commit d695c12c57fe ("[media] media: s5p-mfc fix invalid memory access
    from s5p_mfc_release()") tried to solve a problem of driver's remove
    callback being called while userspace still holds references to objects
    provided by the driver. However, the only case it solves is when
    userspace closes related file descriptors. If it tries to do something
    else, such as calling IOCTLs, it will still cause a crash, because the
    associated video_device would be freed already by
    video_device_release(), which is a simple wrapper over kfree().
    
    Another aspect of the problem is that the commit in question bypassed
    cleanup steps necessary to cleanly unload the driver, such as freeing
    instance buffers.
    
    Revert the commit in question and add a workaround in terms of
    setting suppress_bind_attrs in the platfor_driver struct, so that it is
    only possible to trigger the remove callback by module unload, which
    cannot happen if userspace holds any references to the module, e.g. open
    files.
    
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/media/platform/s5p-mfc/s5p_mfc.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc.c
@@ -937,53 +937,42 @@ static int s5p_mfc_release(struct file *file)
 	struct s5p_mfc_ctx *ctx = fh_to_ctx(file->private_data);
 	struct s5p_mfc_dev *dev = ctx->dev;
 
-	/* if dev is null, do cleanup that doesn't need dev */
 	mfc_debug_enter();
-	if (dev)
-		mutex_lock(&dev->mfc_mutex);
+	mutex_lock(&dev->mfc_mutex);
 	vb2_queue_release(&ctx->vq_src);
 	vb2_queue_release(&ctx->vq_dst);
-	if (dev) {
-		s5p_mfc_clock_on();
+	s5p_mfc_clock_on();
 
-		/* Mark context as idle */
-		clear_work_bit_irqsave(ctx);
-		/*
-		 * If instance was initialised and not yet freed,
-		 * return instance and free resources
-		*/
-		if (ctx->state != MFCINST_FREE && ctx->state != MFCINST_INIT) {
-			mfc_debug(2, "Has to free instance\n");
-			s5p_mfc_close_mfc_inst(dev, ctx);
-		}
-		/* hardware locking scheme */
-		if (dev->curr_ctx == ctx->num)
-			clear_bit(0, &dev->hw_lock);
-		dev->num_inst--;
-		if (dev->num_inst == 0) {
-			mfc_debug(2, "Last instance\n");
-			s5p_mfc_deinit_hw(dev);
-			del_timer_sync(&dev->watchdog_timer);
-			s5p_mfc_clock_off();
-			if (s5p_mfc_power_off() < 0)
-				mfc_err("Power off failed\n");
-		} else {
-			mfc_debug(2, "Shutting down clock\n");
-			s5p_mfc_clock_off();
-		}
+	/* Mark context as idle */
+	clear_work_bit_irqsave(ctx);
+	/* If instance was initialised and not yet freed,
+	 * return instance and free resources */
+	if (ctx->state != MFCINST_FREE && ctx->state != MFCINST_INIT) {
+		mfc_debug(2, "Has to free instance\n");
+		s5p_mfc_close_mfc_inst(dev, ctx);
 	}
-	if (dev)
-		dev->ctx[ctx->num] = NULL;
+	/* hardware locking scheme */
+	if (dev->curr_ctx == ctx->num)
+		clear_bit(0, &dev->hw_lock);
+	dev->num_inst--;
+	if (dev->num_inst == 0) {
+		mfc_debug(2, "Last instance\n");
+		s5p_mfc_deinit_hw(dev);
+		del_timer_sync(&dev->watchdog_timer);
+		s5p_mfc_clock_off();
+		if (s5p_mfc_power_off() < 0)
+			mfc_err("Power off failed\n");
+	} else {
+		mfc_debug(2, "Shutting down clock\n");
+		s5p_mfc_clock_off();
+	}
+	dev->ctx[ctx->num] = NULL;
 	s5p_mfc_dec_ctrls_delete(ctx);
 	v4l2_fh_del(&ctx->fh);
-	/* vdev is gone if dev is null */
-	if (dev)
-		v4l2_fh_exit(&ctx->fh);
+	v4l2_fh_exit(&ctx->fh);
 	kfree(ctx);
 	mfc_debug_leave();
-	if (dev)
-		mutex_unlock(&dev->mfc_mutex);
-
+	mutex_unlock(&dev->mfc_mutex);
 	return 0;
 }
 
@@ -1409,25 +1398,8 @@ err_dma:
 static int s5p_mfc_remove(struct platform_device *pdev)
 {
 	struct s5p_mfc_dev *dev = platform_get_drvdata(pdev);
-	struct s5p_mfc_ctx *ctx;
-	int i;
 
 	v4l2_info(&dev->v4l2_dev, "Removing %s\n", pdev->name);
-
-	/*
-	 * Clear ctx dev pointer to avoid races between s5p_mfc_remove()
-	 * and s5p_mfc_release() and s5p_mfc_release() accessing ctx->dev
-	 * after s5p_mfc_remove() is run during unbind.
-	*/
-	mutex_lock(&dev->mfc_mutex);
-	for (i = 0; i < MFC_NUM_CONTEXTS; i++) {
-		ctx = dev->ctx[i];
-		if (!ctx)
-			continue;
-		/* clear ctx->dev */
-		ctx->dev = NULL;
-	}
-	mutex_unlock(&dev->mfc_mutex);
 
 	del_timer_sync(&dev->watchdog_timer);
 	flush_work(&dev->watchdog_work);
@@ -1634,6 +1606,7 @@ static struct platform_driver s5p_mfc_driver = {
 	.driver	= {
 		.name	= S5P_MFC_NAME,
 		.pm	= &s5p_mfc_pm_ops,
+		.suppress_bind_attrs = true,
 		.of_match_table = exynos_mfc_match,
 	},
 };
